e30aaba73596bcd01d3dc5bc63849edbf05a25cf
Paulluss <130979446+Paulluss@users.noreply.github.com>
2023-05-09 13:14:59 +0530

    contract.sol

--- NFT MarketPlace.api/contract.sol
+++ NFT MarketPlace.api/contract.sol
@@ -4,8 +4,6 @@ pragma solidity ^0.8.9;
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
-import "hardhat/console.sol";
-
 
 contract NFTMarketPlace is ERC721URIStorage {
    using Counters for Counters.Counter;
